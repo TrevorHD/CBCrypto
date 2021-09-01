ae7992d28fc854f0292341357943391f292ae214
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-08-31 21:08:45 -0400

    File upload 4
    Uploaded a script that runs the program in its entirety.

---
+++ 00_RunAll.py
@@ -0,0 +1,14 @@
+##### Run CBCrypto ----------------------------------------------------------------------------------------
+
+# Import packages
+exec(open("01_Imports.py").read())
+
+# Set up API functions
+exec(open("02_CoreAPI.py").read())
+
+# Set up GUI functions
+exec(open("03_CoreGUI.py").read())
+
+# Run GUI
+exec(open("04_RunGUI.py").read())
+

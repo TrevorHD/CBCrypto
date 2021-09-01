35c1ec6a17778500e18d6cac6119b0517f0726a4
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-08-31 23:58:23 -0400

    File restructure 1
    Moved all of the scripts into their own folder.

--- Scripts/00_RunAll.py
+++ Scripts/00_RunAll.py
@@ -1,14 +1,14 @@
 ##### Run CBCrypto ----------------------------------------------------------------------------------------
 
 # Import packages
-exec(open("01_Imports.py").read())
+exec(open("Scripts/01_Imports.py").read())
 
 # Set up API functions
-exec(open("02_CoreAPI.py").read())
+exec(open("Scripts/02_CoreAPI.py").read())
 
 # Set up GUI functions
-exec(open("03_CoreGUI.py").read())
+exec(open("Scripts/03_CoreGUI.py").read())
 
 # Run GUI
-exec(open("04_RunGUI.py").read())
+exec(open("Scripts/04_RunGUI.py").read())
 
